bc02767f150a8f3b60438b25adbe47292e6d5e19
Zalina1991 <110950035+Zalina1991@users.noreply.github.com>
2022-10-26 07:17:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Hello world!", 'Hi!')
+a = 5
+b = 3
+print("a")
+print('a=5')
+c = a * b
+print('c = ', c)
+print('a + c =', a + c)
+print('a =', a)
+print('b =', b)
+if a > b:
+   print('a > b')  
+else: 
+   print('a < b')
+a = 5
+b = 7
+c = 10
+max = 0
+print('a =', a)
+print('b =', b)
+print('c =', c)
+if max < a:
+   max = a
+if max < b:
+   max = b
+if max < c:
+  max = c
+print('max = ', max)
